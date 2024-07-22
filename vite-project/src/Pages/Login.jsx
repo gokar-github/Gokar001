2cc8363ab97b84267d5ddedbb64518f6ed584526
Lauric1 <Lauricgokar@gmail.com>
2024-07-22 11:14:31 +0200

    ok

---
+++ vite-project/src/Pages/Login.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+
+
+function Login() {
+    return (
+        <>
+            <div className='container'>
+                <div className='form'></div>
+                <div className='image'></div>
+            </div>
+        </>
+
+    );
+
+}
